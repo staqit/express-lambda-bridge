599f1e359b898c097e34a1c68a7b0c6587394dcb
Tiaan Louw <tiaanl@gmail.com>
2019-12-15 17:11:22 +0100

    First code commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as lambda from './lambda';
+
+module.exports = {
+  ...lambda
+};
---
+++ src/lambda.ts
@@ -0,0 +1,5 @@
+import {Request, Response} from 'express';
+
+export const callLambdaFunction = async (request: Request, response: Response) => {
+  console.log('testing');
+};
